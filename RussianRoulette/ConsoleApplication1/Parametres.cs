a8b4efafaee36762d63a462dbe919673c6888144
E Laffont <laffont.edu@gmail.com>
2024-10-08 12:55:22 +0200

    Add min/max pour chambres et balles

--- RussianRoulette/ConsoleApplication1/Parametres.cs
+++ RussianRoulette/ConsoleApplication1/Parametres.cs
@@ -7,7 +7,23 @@ namespace ConsoleApplication1
         public void parametres()
         {
             int num_chambres = int.Parse(Console.ReadLine());
+            if (num_chambres < 6)
+            {
+                Console.Write("minimun 6 chambres");
+            } 
+            else if (num_chambres > 15)
+            {
+                Console.Write("maximun 15 chambres");
+            }
             int num_balles_chambres = int.Parse(Console.ReadLine());
+            if (num_balles_chambres <= 1)
+            {
+                Console.Write("minimun 1 balle");
+            }
+            else if (num_balles_chambres > num_chambres - 1)
+            {
+                Console.Write("il ne peut pas avoir plus de balles que de chambres");
+            }
         }
     }
 }
