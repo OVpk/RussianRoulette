d3250ecc9bb3fd787f59f4f2cf3992c5dfb90802
Michel <mchl.fran04@gmail.com>
2024-10-08 14:38:58 +0200

    j et t

--- RussianRoulette/ConsoleApplication1/Joueur.cs
+++ RussianRoulette/ConsoleApplication1/Joueur.cs
@@ -1,7 +1,42 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Data.SqlTypes;
+
+namespace ConsoleApplication1
 {
     public class Joueur
     {
+        public string pseudoJoueur1;
+        public string pseudoJoueur2;
+        public bool j1Prio;
+        public bool j2Prio;
         
+        public void Role()
+        {
+            Console.WriteLine("entrée nom du J1 : ");
+            pseudoJoueur1 = Console.ReadLine();
+            
+            Console.WriteLine("entrée nom du J2 : ");
+            pseudoJoueur2 = Console.ReadLine();
+
+            Random rnd = new Random();
+            
+            
+            if (rnd.Next(2) == 0)
+            {
+                Console.WriteLine("J1");
+            }
+            else 
+            {
+                Console.WriteLine("J2");
+            }
+            
+                
+                
+        }
+            
     }
+        
+        
+    
 }
