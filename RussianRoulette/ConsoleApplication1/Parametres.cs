144d1cece13924e381b984402066023cd68cc344
E Laffont <laffont.edu@gmail.com>
2024-10-08 12:45:59 +0200

    Paramétres définis

--- RussianRoulette/ConsoleApplication1/Parametres.cs
+++ RussianRoulette/ConsoleApplication1/Parametres.cs
@@ -1,7 +1,13 @@
-﻿namespace ConsoleApplication1
+﻿using System;
+
+namespace ConsoleApplication1
 {
     public class Parametres
     {
-        
+        public void parametres()
+        {
+            int num_chambres = int.Parse(Console.ReadLine());
+            int num_balles_chambres = int.Parse(Console.ReadLine());
+        }
     }
 }
